992157ad7bbb3c4d706722e2f10115a1eb8649a6
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-03-15 19:06:34 -0400

    added animation on route changes

--- app/js/owm-app.js
+++ app/js/owm-app.js
@@ -1,39 +1,57 @@
-//declare an Angular module called 'OWMApp' & inject ngRoute as a dependency
-angular.module('OWMApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider){
-        $routeProvider.when('/', { //when user requests the root url(nothing after the /), respond w/ home.html template & use 'HomeCtrl' controller
-            templateUrl : 'home.html',
-            controller : 'HomeCtrl'
-        }).when('/cities/:city', { //when user requests the url ending in /city, respond w/ city.html template & use 'CityCtrl' controller
-        	templateUrl : 'city.html',
-        	controller : 'CityCtrl'
-        	resolve: {
-        		city: function(owmCities, $route, $location) {
-        			var city = $route.current.params.city; 
-        			if(owmCities.indexOf(city) === -1){ //if city not found throw an error
-        				$location.path('/error'); //use the $location service to redirect to the error page when matching city not found
-        				return;
-        			}
-        			return city;
-        		}
-        	}
-        	.when('/error', {
-        		template : '<p>Error - Page Not Found</p>' //use template property to supply an in-line template error page(not a best practice, just for example)
-        	})
-        	.run(function($rootScope, $location){ //if route not found or resolver fails, redirect to /error route
-        		$rootScope.$on('$routeChangeError', function() {
-        			$location.path('/error');
-        		})
-        	});
-        });
-    }])
-    //declare a controller called 'HomeCtrl'
-    .controller('HomeCtrl', function($scope) {
-        //empty for now
-    })
-    //declare a controller called 'CityCtrl'
-    .controller('CityCtrl', function($scope, city) { //city refers to line 12
-    	$scope.city = city; 
-    }]);
+//declare Angular module called 'OWMApp' & inject ngRoute as a dependency
+angular.module( 'OWMApp', ['ngRoute', 'ngAnimate'] )
 
-    .value('owmCities', ['New York', 'Dallas', 'Chicago'])
+    .value( 'owmCities', [
+        'New York',
+        'Dallas',
+        'Chicago',
+    ])
+
+    .config(function( $routeProvider ) {
+        $routeProvider
+            .when( '/', { //when root url requested(nothing after /) respond w/ home.html & HomeCtrl
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl as home'
+            })
+            .when( '/error', { template: '<p>Error: Page not found</p>' }) //use template property to supply in-line template error page
+            .when( '/cities/:city', { //when /city url requested respond w/ city.html & CityCtrl
+                templateUrl: 'city.html',
+                controller: 'CityCtrl',
+                resolve: {
+                    city: function( owmCities, $route, $location ) {
+                        var city = $route.current.params.city;
+
+                        if ( owmCities.indexOf( city ) === -1 ) { //if city not found throw error
+                            $location.path( '/error' ); //use $location service to redirect to error page if matching city not found
+                            return;
+                        }
+                        return city;
+                    }
+                }
+            })
+
+            .otherwise({ redirectTo: '/error' });
+    })
+    //if route not found or resolver fails, redirect to error route
+    .run(function($rootScope, $location, $timeout) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function() {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+          $timeout(function() {
+            $rootScope.isLoading = false;
+          }, 1000);
+        });
+    })
+
+    //declare a controller called HomeCtrl
+    .controller( 'HomeCtrl', function( $scope ) {
+        this.welcomeMessage = 'Welcome home';
+    })
+    //declare a controller called CityCtrl
+    .controller( 'CityCtrl', function( $scope, city ) { //city parameter refers to line 12
+        $scope.city = city;
+    });
